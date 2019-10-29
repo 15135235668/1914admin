5cd53b2ba53bfa0e68ddd0634ed599833de5d87d
unknown <15135235668>
2019-10-29 20:53:05 +0800

    zcinit

---
+++ admin/src/demo/less/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.less'
+function Less() {
+    return (
+        <div className="less">
+            <h3>less demo</h3>
+        </div>
+    );
+}
+
+export default Less;
